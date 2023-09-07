88ef7eda16cee17abe1147906ecb0ab708031bf8
Matteo Falasca <matteo.falasca@emmek.org>
2023-09-07 17:22:56 +0200

    delete

--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -1,7 +1,34 @@
-import { ListGroup } from "react-bootstrap";
+import { Button, ListGroup } from "react-bootstrap";
+
+const deleteComment = async (event, id) => {
+  console.log("DELETE", id);
+
+  const url = "https://striveschool-api.herokuapp.com/api/comments/";
+  const options = {
+    method: "DELETE",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NTIyMGMwMzRmZjAwMTQwM2Y0Y2QiLCJpYXQiOjE2OTQwODk3MTgsImV4cCI6MTY5NTI5OTMxOH0.yy5_J1EHIdfBE0x6pZgPJ2RrplUDZE2vU6TvoY2MdDM",
+    },
+  };
+
+  try {
+    const response = await fetch(url + id, options);
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const SingleComment = props => {
-  return <ListGroup.Item>{props.comment.comment}</ListGroup.Item>;
+  return (
+    <ListGroup.Item className="d-flex">
+      <span className="me-auto">{props.comment.comment}</span>
+      <Button variant="danger" size="sm" onClick={event => deleteComment(event, props.comment._id)}>
+        <i className="bi bi-trash"></i>
+      </Button>
+    </ListGroup.Item>
+  );
 };
 
 export default SingleComment;
